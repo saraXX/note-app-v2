ff66bda08c30c98bd717fd2e729a0cd815c59d30
saraXX <sara99.edu@gmail.com>
2022-10-26 01:23:18 +0300

    remove unnecessary

--- app/src/main/java/android/guide/note_app_v2/MainActivity.kt
+++ app/src/main/java/android/guide/note_app_v2/MainActivity.kt
@@ -1,14 +1,10 @@
 package android.guide.note_app_v2
 
 import android.guide.note_app_v2.databinding.ActivityMainBinding
-import android.guide.note_app_v2.fragments.NoteListFragmentDirections
-import android.guide.note_app_v2.viewmodel.NoteViewModel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.activityViewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
@@ -22,14 +18,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        // Retrieve NavController from the NavHostFragment
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
+
+        // Set up the action bar for use with the NavController
         setupActionBarWithNavController(navController)
 
     }
-
+    /**
+     * Handle navigation when the user chooses Up from the action bar.
+     */
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
